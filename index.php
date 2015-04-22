c6ea9e971e1b1ea478d6a67dad1a57a41390b43f
Dorie Lincoln <lincolndorie@gmail.com>
2015-04-22 15:16:01 -0700

    Gecko Index Page

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html> 
+
+	<html> 
+		<head> 
+			<title>Gecko Wine Company</title> 
+				<meta charset="utf-8" /> <meta name="robots" content="noindex,nofollow"  /> 
+				<meta name="viewport" content="width=device-width" /> 
+				<link href="css/styles.css" rel="stylesheet" type="text/css" /> 
+				<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script> 
+				<script src="script.js"></script> 
+				
+	    </head>
+
+	<body>
+	 
+  <!-- HEADER START --> 
+  	<header>
+  		  
+  		 	<div class="logo"> 
+  		 		<img src="img/logo.jpg" width="900px"  height="500px" />
+  			</div>
+  		 
+  <!-- NAVIGATION START --> 
+  		<div id='cssmenu'>
+			<ul>
+  			 	<li class='active'><a href='#'><span>About</span></a></li>
+  			 	<li class='has-sub'><a href='#'><span>Wine</span></a>
+      		<ul>
+         		<li><a href='#'><span>12th Man</span></a></li>
+         		<li><a href='#'><span>Malbec</span></a></li>
+         		<li><a href='#'><span>Pino Gris</span></a></li>
+         		<li class='last'><a href='#'><span>Rose</span></a></li>
+      		</ul>
+  				</li>
+  				<li class='active'><a href='#'><span>Events</span></a></li>
+   				<li class='has-sub'><a href='#'><span>Latest Releases</span></a>
+            <ul>
+         		<li><a href='#'><span>Spring 2015</span></a></li>
+        		<li class='last'><a href='#'><span>Fall 2015</span></a></li>
+      	    </ul>
+   		   		</li>
+   		   		<li class='active'><a href='#'><span>Recipes</span></a><li>
+   				<li class='last'><a href='#'><span>Contact</span></a></li>
+		   </ul>
+		</div>
+
+<!-- NAVIGATION END --> 
+
+ </header>
+
+<!-- MAIN CONTENT START -->
+		<aside>
+		
+		<div class="main">
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Nulla venenatis, enim vel ullamcorper rhoncus, ex nunc blandit
+			velit, eget tempor nisi lectus sed nunc. In egestas massa ut 
+			turpis imperdiet, eu bibendum dui tempor. Pellentesque finibus, 
+			massa id ullamcorper scelerisque, libero neque nec dolor. 
+			Integer feugiat ut purus non vulputate. Curabitur et metus eget 
+			lectus placerat venenatis nec eget eros. Pellentesque habitant
+			morbi tristique senectus et netus Nullam a cursus felis. Cras
+			imperdiet a velit egestas cursus. Etiam augue urna, tempus at 
+			odio eu, porta tristique ex. Nunc dictum sapien porttitor leo 
+			consequat vulputate. Nullam porttitor placerat dolor vel luctus. 
+			Donec auctor scelerisque sagittis. Nullam venenatis aliquam mi
+			d ultrices. Ut turpis orci, vulputate in dui nec, posuere 
+             scelerisque lorem.</p>
+
+            <p> Duis eu tempus nulla. Suspendisse a lorem ut est ultricies 
+            suscipit. In sagittis dui t eleifend pulvinar. Fusce feugiat 
+            elementum leo, nec vestibulum metus pharetra nec. Curabitur
+            egestas volutpat mattis. Suspendisse faucibus feugiat lacus 
+            ac condimentum. Proin fringilla in ipsum malesuada consequat.
+            Curabitur sem dolor, dignissim et diam at, condimentum rhoncus dui. </p>
+             
+		</div>	
+		</aside>
+	
+		
+		
+  <!-- FOOTER START --> 
+  			<hr> </hr>
+  			<br> </br>
+  		<footer> 
+  				<small> &copy; 2015, All Rights Reserved Gecko Wine Company~ Designed & Authored by Dorie Lincoln ~
+  				
+  				 </small> 
+  		</footer>
+  <!-- FOOTER END -->
+
+</body> 
+
+</html>
